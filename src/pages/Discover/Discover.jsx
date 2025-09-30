d75e922d01f219db8744c28c9e250383e5ec9241
Furkan-ThecLifeN <furkantheclifen@gmail.com>
2025-10-01 01:28:06 +0300

    Test save 2

--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -7,9 +7,17 @@ import BottomNav from "../../components/BottomNav/BottomNav";
 const Discover = () => {
   return (
     <div className={styles.discover}>
-      <Sidebar />
-      <ExploreFeed />
-      <BottomNav />
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+
+      <div className="exploreFeed">
+        <ExploreFeed />
+      </div>
+
+      <div className="bottomNav">
+        <BottomNav />
+      </div>
     </div>
   );
 };
